4756f24b338ca8c58f07d60801e55333ec47e339
Martin Kielhorn <kielhorn.martin@gmail.com>
2013-11-18 16:10:47 +0100

    camera image can be displayed, start to implement fft

--- andor.lisp
+++ andor.lisp
@@ -1,67 +1,4 @@
-ccl::*shared-libraries*
-ccl::*eeps* ;; hash table with external functions
-
-(progn (use-interface-dir :andor)
-	(open-shared-library "libandor-emu.so"))
-(progn
- 
- (defun get-available-cameras ()
-   (rlet ((n :int))
-     (assert (= #$DRV_SUCCESS
-		(#_GetAvailableCameras
-		 (%int-to-ptr (%address-of n)))))
-     (%get-signed-long (%int-to-ptr (%address-of n)))))
- (defun get-camera-handle (&optional (i 0))
-   (rlet ((n :int))
-     (assert (= #$DRV_SUCCESS
-		(#_GetCameraHandle i (%int-to-ptr (%address-of n)))))
-     (%get-signed-long (%int-to-ptr (%address-of n)))))
- (defun set-current-camera (handle)
-   (#_SetCurrentCamera handle))
- (defun initialize (&optional (dir "/usr/local/etc/andor/"))
-   (with-cstrs ((cdir dir))
-     (assert (= #$DRV_SUCCESS (#_Initialize cdir)))))
- (defun start-acquisition ()
-   (assert (= #$DRV_SUCCESS (#_StartAcquisition))))
- (defun get-status ()
-   (rlet ((status :int))
-     (assert (= #$DRV_SUCCESS
-		(#_GetStatus
-		 (%int-to-ptr (%address-of status)))))
-     (%get-signed-long (%int-to-ptr (%address-of status)))))
- (defun get-acquired-data16 ()
-   (let* ((w 512)
-	  (h 512)
-	  (n (* w h)))
-     (multiple-value-bind (a ap) (make-heap-ivector n ;:unsigned-halfword 
-						    '(unsigned-byte 16)
-						    )
-       (let ((a (#_GetAcquiredData16 ap n)))
-	 (unless (= a #$DRV_SUCCESS)
-	   (break "GetAcquiredData16 returned ~a." a)))
-       (prog1 
-	   (let* ((b1 (make-array n :element-type '(unsigned-byte 16)))
-		  (b (make-array (list h w) :element-type '(unsigned-byte 16)
-				 :displaced-to b1)))
-	     (dotimes (i n)
-	       (setf (aref b1 i) (aref a i)))
-	     b)
-	 (dispose-heap-ivector a))))))
-#+nil
-(progn
-  (initialize)
-  (start-acquisition)
-  (external-call "print_cam" :void)
-  (get-status)
-  (external-call "print_cam" :void)
-  (defparameter *bla*   (get-acquired-data16))
-    (external-call "print_cam" :void))
-
-#+nil
-(external-call "print_cam" :void)
-
 (load "/home/martin/quicklisp/setup.lisp")
-
 (progn
   (ql:quickload :cl-glut)
   (ql:quickload :cl-opengl)
@@ -71,6 +8,269 @@ ccl::*eeps* ;; hash table with external functions
 
 (in-package :g)
 
+(use-interface-dir :fftw3)
+(open-shared-library "libfftw3.so")
+(defun ft (a)
+  (destructuring-bind (h w) (array-dimensions a)
+    (multiple-value-bind (ind indp) (ccl:make-heap-ivector 2 'fixnum)
+      (setf (aref ind 0) h
+	    (aref ind 1) w)
+      (multiple-value-bind (b bp) (ccl:make-heap-ivector (* 2 h w) 'double-float)
+	(let ((a1 (make-array (* w h) :element-type (array-element-type a)
+			      :displaced-to a)))
+	  (if (complexp (aref a1 0))
+	      (dotimes (i (length a1))
+		(setf (aref b (* 2 i))       (* 1d0 (realpart (aref a1 i)))
+		      (aref b (+ 1 (* 2 i))) (* 1d0 (imagpart (aref a1 i)))))
+	      (dotimes (i (length a1))
+		(setf (aref b (* 2 i))       (* 1d0 (aref a1 i))
+		      (aref b (+ 1 (* 2 i))) 0d0))))
+	(multiple-value-bind (c cp) (ccl:make-heap-ivector (* 2 h w) 'double-float)	
+	  (prog1
+	     (progn
+	       (let ((plan (#_fftw_plan_dft 2 indp bp cp #$FFTW_FORWARD #$FFTW_ESTIMATE)))
+		 (#_fftw_execute plan))
+	       (let* ((d (make-array (array-dimensions a)
+				     :element-type '(complex double-float)))
+		      (d1 (make-array (* w h) :element-type (array-element-type d)
+				      :displaced-to d)))
+		 (dotimes (i (length d1))
+		   (setf (aref d1 i) (complex (aref c (* 2 i))       
+					     (aref c (+ 1 (* 2 i)))) ))
+		 d))
+	    (ccl:dispose-heap-ivector b)
+	    (ccl:dispose-heap-ivector c)))))))
+
+(defparameter *kbla* (ft *bla*))
+
+
+
+
+;;ccl::*shared-libraries*
+;;ccl::*eeps* ;; hash table with external functions
+
+(progn (use-interface-dir :andor)
+       (open-shared-library "libandor.so"))
+(progn
+  (defun get-available-cameras ()
+    (rlet ((n :int))
+      (assert (= #$DRV_SUCCESS
+		 (#_GetAvailableCameras
+		  (%int-to-ptr (%address-of n)))))
+      (%get-signed-long (%int-to-ptr (%address-of n)))))
+  (defun get-camera-handle (&optional (i 0))
+    (rlet ((n :int))
+      (assert (= #$DRV_SUCCESS
+		 (#_GetCameraHandle i (%int-to-ptr (%address-of n)))))
+      (%get-signed-long (%int-to-ptr (%address-of n)))))
+  (defun set-current-camera (handle)
+    (#_SetCurrentCamera handle))
+  (defun initialize (&optional (dir "/usr/local/etc/andor"))
+    (with-cstrs ((cdir dir))
+      (let ((r (#_Initialize cdir)))
+	(unless (= #$DRV_SUCCESS r)
+	  (break "Initialize returned ~a." r)))))
+  (defun start-acquisition ()
+    (assert (= #$DRV_SUCCESS (#_StartAcquisition))))
+  (defun get-status ()
+    (rlet ((status :int))
+      (assert (= #$DRV_SUCCESS
+		 (#_GetStatus
+		  (%int-to-ptr (%address-of status)))))
+      (%get-signed-long (%int-to-ptr (%address-of status)))))
+  (defun set-read-mode (&optional (n 4))
+    (assert (= #$DRV_SUCCESS (#_SetReadMode n))))
+  (defun set-image (&key (hbin 1) (vbin 1) (hstart 1) (hend 1024) (vstart 1) (vend 1024))
+    (assert (= #$DRV_SUCCESS (#_SetImage hbin vbin hstart hend vstart vend))))
+  (defun set-exposure-time (&optional (time_s .1s0))
+    (assert (= #$DRV_SUCCESS (#_SetExposureTime time_s))))
+  (defun set-ad-channel (&optional (channel 0))
+    (assert (= #$DRV_SUCCESS (#_SetADChannel channel))))
+  (defun get-number-amp ()
+    (rlet ((n :int))
+      (assert (= #$DRV_SUCCESS
+		 (#_GetNumberAmp
+		  (%int-to-ptr (%address-of n)))))
+      (%get-signed-long (%int-to-ptr (%address-of n)))))
+
+  (defun get-number-available-images ()
+    (rlet ((first :int)
+	   (last :int))
+      (let ((r (#_GetNumberAvailableImages
+		(%int-to-ptr (%address-of first))
+		(%int-to-ptr (%address-of last)))))
+	(unless (= #$DRV_SUCCESS r)
+	  (break "GetNumberAvailableImages returned ~a." r)))
+      
+      (list
+       (%get-signed-long (%int-to-ptr (%address-of first)))
+       (%get-signed-long (%int-to-ptr (%address-of last))))))
+
+
+  (defun get-number-hs-speeds (&key (channel 1) (typ 1))
+    (rlet ((speeds :int))
+      (let ((r (#_GetNumberHSSpeeds channel typ 
+				    (%int-to-ptr (%address-of
+						  speeds)))))
+	(unless (= #$DRV_SUCCESS r)
+	  (break "GetNumberHSSpeeds returned ~a." r)))
+      (%get-signed-long (%int-to-ptr (%address-of speeds)))))
+
+  (defun get-hs-speed (&key (channel 1) (typ 1) (index 0))
+    (rlet ((speeds :float))
+      (let ((r (#_GetHSSpeed channel typ index
+			     (%int-to-ptr (%address-of
+					   speeds)))))
+	(unless (= #$DRV_SUCCESS r)
+	  (break "GetHSSpeed returned ~a." r)))
+      (%get-single-float (%int-to-ptr (%address-of speeds)))))
+
+  (defun set-hs-speed (&key (typ 1) (index 0))
+    (let ((r (#_SetHSSpeed typ index)))
+      (unless (= #$DRV_SUCCESS r)
+	(break "SetHSSpeed returned ~a." r))))
+  (defun abort-acquisition ()
+    (assert (= #$DRV_SUCCESS (#_AbortAcquisition))))
+
+  (defun get-acquisition-timings ()
+    (rlet ((exposure :float)
+	   (accumulate :float)
+	   (kinetic :float))
+      (let ((r (#_GetAcquisitionTimings (%int-to-ptr (%address-of exposure))
+					(%int-to-ptr (%address-of accumulate))
+					(%int-to-ptr (%address-of kinetic)))))
+	(unless (= #$DRV_SUCCESS r)
+	  (break "GetAcquisitionTimings returned ~a." r)))
+      (list (%get-single-float (%int-to-ptr (%address-of exposure)))
+	    (%get-single-float (%int-to-ptr (%address-of accumulate)))
+	    (%get-single-float (%int-to-ptr (%address-of kinetic))))))
+
+  (defun free-internal-memory ()
+    (assert (= #$DRV_SUCCESS (#_FreeInternalMemory))))
+  
+  (defun get-number-ad-channels ()
+    (rlet ((n :int))
+      (assert (= #$DRV_SUCCESS
+		 (#_GetNumberADChannels
+		  (%int-to-ptr (%address-of n)))))
+      (%get-signed-long (%int-to-ptr (%address-of n)))))
+
+  (defun shut-down ()
+    (assert (= #$DRV_SUCCESS (#_ShutDown))))
+  (defun set-acquisition-mode (&optional (mode 1))
+    (let ((r (#_SetAcquisitionMode mode)))
+      (unless (= r #$DRV_SUCCESS)
+	(break "SetAcquisitionMode returned ~a." r))))
+  (defun get-acquired-data16 ()
+    (let* ((w 1024)
+	   (h 1024)
+	   (n (* w h)))
+      (multiple-value-bind (a ap) (make-heap-ivector n ;:unsigned-halfword 
+						     '(unsigned-byte 16)
+						     )
+	(let ((r (#_GetAcquiredData16 ap n)))
+	  (unless (= r #$DRV_SUCCESS)
+	    (break "GetAcquiredData16 returned ~a." r)))
+	(prog1 
+	    (let* ((b1 (make-array n :element-type '(unsigned-byte 16)))
+		   (b (make-array (list h w) :element-type '(unsigned-byte 16)
+				  :displaced-to b1)))
+	      (dotimes (i n)
+		(setf (aref b1 i) (aref a i)))
+	      b)
+	  (dispose-heap-ivector a))))))
+
+
+
+
+#+nil
+(list (get-number-ad-channels)
+      (get-number-amp)
+      (get-number-hs-speeds :channel 1)
+      (get-hs-speed :channel 1 :index 0))
+
+
+#+nil
+(set-hs-speed :typ 1 :index 0)
+
+#+nil
+(shut-down)
+
+
+
+#+nil
+(progn
+  (initialize)
+  (set-read-mode)
+  (set-image :hend 1024 :vend 1024)
+  (set-ad-channel 1)
+  (set-exposure-time .00001)
+  (set-acquisition-mode 1)
+  (set-hs-speed :typ 1 :index 0)
+  (get-acquisition-timings))
+
+#+nil
+(get-number-available-images)
+
+(defparameter *acquire-p* t)
+
+#+nil
+(abort-acquisition)
+
+#+nil
+(get-status)
+
+(defun get-time ()
+ (rlet ((tv :timeval))
+   (#_gettimeofday tv (%null-ptr))
+   (+ (* 1000000 (pref tv :timeval.tv_sec))
+      (pref tv :timeval.tv_usec))))
+
+#+nil
+(get-time)
+
+(defun acquire-one-image ()
+  (start-acquisition) 
+  (let ((start (get-time)))
+   (loop while (/= #$DRV_IDLE (get-status)) do
+	(sleep .01))
+   (format t "aquisition took ~a us~%" (- (get-time) start)))
+  (defparameter *bla* 
+    (get-acquired-data16))
+  (free-internal-memory)
+  (sleep .01))
+
+#+nil
+(acquire-one-image)
+
+#+nil
+(ccl:process-run-function "acquisition" 
+			  #'(lambda ()
+			      (loop while *acquire-p* do
+				   (acquire-one-image))))
+#+nil
+(initialize)
+
+#+nil
+(get-status)
+
+#+nil
+(set-read-mode)
+
+#+nil
+(set-image :hend 512 :vend 512)
+
+#+nil
+(set-exposure-time .01)
+
+;; sudo mount --bind /dev/bus/ /proc/bus/      
+;;cd /etc/udev/rules.d
+;;sed -i -e s/SYSFS/ATTR/g *.rules
+#+nil
+(external-call "print_cam" :void)
+
+
+
 (let ((rot 0))
  (defun draw ()
    (clear :color-buffer :depth-buffer)
@@ -79,10 +279,10 @@ ccl::*eeps* ;; hash table with external functions
    (if (< 360 rot)
        (setf rot 0))
    (with-pushed-matrix
-     (translate 0 0 -5)
-     (rotate rot 1 0 0)
-     (line-width 4)
-     (let ((r 1.5))
+     (translate -1.2 -2.4 -5)
+     (rotate (* 3 rot) 1 0 0)
+     (line-width 2)
+     (let ((r .3))
        (glut:wire-sphere r 15 7)
        (color 0 0 0 1)
        (glut:solid-sphere (* .99 r) 15 7)))
@@ -93,21 +293,21 @@ ccl::*eeps* ;; hash table with external functions
      (rect .14 -3 -.14 3))
    (with-pushed-matrix
      (translate -.5 -.5 -1.4)
+     (scale 1 1 1)
      (color 1 1 1)
      (let ((obj (first (gen-textures 1))))
        (bind-texture :texture-2d obj)
        
-       (tex-parameter :texture-2d :texture-min-filter :nearest)
-       (tex-parameter :texture-2d :texture-mag-filter :nearest)
-       (start-acquisition) 
-       (let ((b1 (make-array (* 512 512) :element-type '(unsigned-byte 16)
-			     :displaced-to (get-acquired-data16))))
+       (tex-parameter :texture-2d :texture-min-filter :linear)
+       (tex-parameter :texture-2d :texture-mag-filter :linear)
+       (let ((b1 (make-array (* 1024 1024) :element-type '(unsigned-byte 16)
+			     :displaced-to *bla*)))
 	 (multiple-value-bind (a ap)
-	     (make-heap-ivector (* 512 512)
+	     (make-heap-ivector (* 1024 1024)
 				'(unsigned-byte 16))
-	   (dotimes (i (* 512 512))
-	     (setf (aref a i) (* 500 (aref b1 i))))
-	   (tex-image-2d :texture-2d 0 :rgba 512 512 0 :green :unsigned-short
+	   (dotimes (i (* 1024 1024))
+	     (setf (aref a i) (min 65535 (max 0 (* 5 (+ -0 (aref b1 i)))))))
+	   (tex-image-2d :texture-2d 0 :rgba 1024 1024 0 :green :unsigned-short
 			 ap) 
 	   (dispose-heap-ivector a)))
        (enable :texture-2d)
@@ -119,13 +319,12 @@ ccl::*eeps* ;; hash table with external functions
        (disable :texture-2d)
        (delete-textures (list obj))))
    (flush)
- ;;  (sleep (/ 61.3))
+   (sleep (/ 61.3))
 ;   (cl-opengl-bindings:wait-sync )
    (glut:post-redisplay)))
 
 
 (progn 
-
   (defclass clip-window (glut:window)
     ()
     (:default-initargs :pos-x 853 :pos-y 0 :width 512 :height 768
